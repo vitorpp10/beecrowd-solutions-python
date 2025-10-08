b7fe8314100cd22a270913c0166e97f4d456f930
Seu nome <seuemail@exemplo.com>
2025-10-08 14:09:54 -0300

    Add if exercise

---
+++ easy exercises/if exercise.py
@@ -0,0 +1,7 @@
+x = input('digite valor de x: ')
+y = input('digite o valor de y: ')
+
+if x > y:
+    print(f'{x} é maior que {y}', ', x é maior que y')
+else:
+    print(f'{x} é menor que {y}', ', x é menor que y')
