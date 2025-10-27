68d4ac180c2f1334fd51636b01da0bbecf0dcff3
vitor <vitorprofissionalpp10@gmail.com>
2025-10-27 15:29:54 -0300

    Create keys.py

---
+++ explicações/keys.py
@@ -0,0 +1,28 @@
+'''
+keys -> x {
+}
+
+estrutura de dados:
+
+pessoa = {
+    'nome': 'Luiz Otávio',
+    'sobrenome': 'Miranda',
+    'idade': 18,
+    'altura': 1.8,
+    'endereços': [
+    {'rua': 'x', 'numero': 1},
+    {'rua': 'x', 'numero': 2}.
+  ]
+}
+
+para acessar a estrutura no print:
+
+print(pessoa['nome'])
+print(pessoa['sobrenome'])
+
+para acessar tudo:
+
+for i in pessoa:
+    print(i)
+
+'''
