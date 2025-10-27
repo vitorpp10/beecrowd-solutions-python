c30ecf76500040d11d5c298823d8d4ea3dca5885
vitor <vitorprofissionalpp10@gmail.com>
2025-10-27 15:28:41 -0300

    Create lambda.py

---
+++ explicações/lambda.py
@@ -0,0 +1,14 @@
+'''
+".sort" ordena uma lista
+
+lambda -> funçao curta é rapida, exemplo
+funçao que soma dois numeros com lambda:
+soma = lambda x, y: x + y
+print(soma(3, 5)), curte é rapido
+output = 8
+
+função que multiplica dois numeros com lambda
+multi = lambda x, y: x * y
+print(multi(1, 2))
+output = 2
+'''
