67be32f2d90b0122dd947218eab97d897c4594a6
vitor <vitorprofissionalpp10@gmail.com>
2025-10-27 15:29:13 -0300

    Create set.py

---
+++ explicações/set.py
@@ -0,0 +1,16 @@
+'''
+set elimina valores duplicados, exemplo:
+lista = ['1', '1', '2', '1']
+sset = set(lista)
+print(sset)
+
+porem o set pode nao garantir a ordem da lista, porem garante que o valor
+será único
+set nao tem indices ou seja nao da pra acessar valor por valor, exemplo:
+sset = {1, 2, 3}
+print(sset[1])
+error 
+
+".add()" adiciona
+".discard()" descarta
+'''
